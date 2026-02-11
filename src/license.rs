9cf62849e06be64929536b47c0f7a9226816f0fe
Nwaokoro Magnus Ikechukwu <thisismagnus2@gmail.com>
2026-02-11 11:28:44 +0000

    pro gating improvement

--- src/license.rs
+++ src/license.rs
@@ -3,14 +3,14 @@ use once_cell::sync::Lazy;
 
 static LICENSE_ACTIVE: Lazy<Mutex<bool>> = Lazy::new(|| Mutex::new(false));
 
-pub fn activate_license(valid: bool) {
+pub fn activate_license() {
     let mut license = LICENSE_ACTIVE.lock().unwrap();
-    *license = valid;
+    *license = true;
 }
 
 pub fn ensure_pro() {
     let license = LICENSE_ACTIVE.lock().unwrap();
     if !*license {
-        panic!("Pro license not activated.");
+        panic!("Pro license not activated. Call optiengine.activate() first.");
     }
 }

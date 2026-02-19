a79de789bbf426f8a103a4bd01839afa007b49c2
Nwaokoro Magnus Ikechukwu <thisismagnus2@gmail.com>
2026-02-19 13:44:13 +0000

    finalised pro gating

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use pyo3::prelude::*;
-
 mod license;
 mod pro;
 
@@ -8,9 +7,14 @@ fn activate_license(key: &str) -> PyResult<()> {
     license::activate_license_py(key)
 }
 
+#[pyfunction]
+fn machine_id() -> PyResult<String> {
+    license::machine_id_py()
+}
+
 #[pymodule]
 fn optiengine(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(activate_license, m)?)?;
-    m.add_class::<pro::adam::Adam>()?;  // ADD THIS
+    m.add_function(wrap_pyfunction!(machine_id, m)?)?;
     Ok(())
 }

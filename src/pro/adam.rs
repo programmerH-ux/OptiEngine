cf8f293b0af8ea8b4d954d9da7ecb4a63511f402
Nwaokoro Magnus Ikechukwu <thisismagnus2@gmail.com>
2026-02-19 12:28:46 +0000

    finalizing pro gating

--- src/pro/adam.rs
+++ src/pro/adam.rs
@@ -1,5 +1,5 @@
 use pyo3::prelude::*;
-use crate::license::ensure_pro()?;
+use crate::license;
 
 #[pyclass]
 pub struct Adam {
@@ -15,19 +15,30 @@ pub struct Adam {
 #[pymethods]
 impl Adam {
     #[new]
-    pub fn new(lr: f64, beta1: f64, beta2: f64, epsilon: f64) -> Self {
-        ensure_pro();
-        Self { lr, beta1, beta2, epsilon, m: 0.0, v: 0.0, t: 0 }
+    pub fn new(lr: f64, beta1: f64, beta2: f64, epsilon: f64) -> PyResult<Self> {
+        license::ensure_pro()?;   // Proper call
+
+        Ok(Self {
+            lr,
+            beta1,
+            beta2,
+            epsilon,
+            m: 0.0,
+            v: 0.0,
+            t: 0,
+        })
     }
 
-    pub fn step(&mut self, value: f64) -> f64 {
-        ensure_pro();
+    pub fn step(&mut self, value: f64) -> PyResult<f64> {
+        license::ensure_pro()?;   // Gate every call
+
         self.t += 1;
         let grad = value;
         self.m = self.beta1 * self.m + (1.0 - self.beta1) * grad;
         self.v = self.beta2 * self.v + (1.0 - self.beta2) * grad * grad;
         let m_hat = self.m / (1.0 - self.beta1.powi(self.t as i32));
         let v_hat = self.v / (1.0 - self.beta2.powi(self.t as i32));
-        value - self.lr * m_hat / (v_hat.sqrt() + self.epsilon)
+
+        Ok(value - self.lr * m_hat / (v_hat.sqrt() + self.epsilon))
     }
 }
